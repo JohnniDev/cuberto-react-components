76b0f286cbd2b1fe1919fbbd315e5b2bb2f44f3e
Цыбульский Сергей <s.tsybulsky@cuberto.ru>
2019-11-06 17:31:59 +0300

    control menu opening from parent component

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -45,6 +45,7 @@ type Props = {
   onOpen: () => void,
 
   // Other
+  isOpened: boolean,
   closeOnSelect: boolean,
   openOnFocus: boolean,
   closeOnControlClick: boolean,
@@ -88,6 +89,7 @@ class Dropdown extends Component<Props, State> {
     onOpen: () => {},
 
     // Other
+    isOpened: null,
     closeOnSelect: true,
     openOnFocus: false,
     closeOnControlClick: true,
@@ -100,7 +102,7 @@ class Dropdown extends Component<Props, State> {
     super(props);
 
     this.state = {
-      open: props.defaultOpen,
+      open: props.isOpened || props.defaultOpen,
       menuStyles: defaultMenuStyles,
     };
   }
@@ -117,6 +119,10 @@ class Dropdown extends Component<Props, State> {
 
   shouldComponentUpdate(nextProps: Props, nextState: State) {
     return !shallowEqual(nextProps, this.props) || !shallowEqual(nextState, this.state);
+  }
+
+  componentWillReceiveProps(nextProps: Props): * {
+    this.toggleMenu(nextProps.isOpened)
   }
 
   getSelectedValue(): Item {
@@ -195,7 +201,7 @@ class Dropdown extends Component<Props, State> {
       evt.preventDefault();
       const customEvent = new KeyboardEvent('keydown', { key: 'Enter', bubbles: true, cancelable: true });
       $firstItem.dispatchEvent(customEvent);
-      this.toggleMenu(false);
+      if(this.props.closeOnSelect) this.toggleMenu(false);
     }
 
     // Focus first/last item
@@ -226,7 +232,7 @@ class Dropdown extends Component<Props, State> {
     if (key === keyboardKey.Enter && item !== null) {
       evt.preventDefault();
       onSelect(evt, item, idx);
-      this.toggleMenu(false);
+      if(this.props.closeOnSelect) this.toggleMenu(false);
     }
 
     // Focus next/prev item
